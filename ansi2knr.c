7a80a2e3fa976a8c8c982953228b65895d7d42c2
UkJung Kim <ujkim@samsung.com>
2012-10-22 16:07:36 +0900

    Git OBS Sync
    Change-Id: I138a134424de44d5ab911109de1dda0a8cd66a92

--- ansi2knr.c
+++ ansi2knr.c
@@ -1,6 +1,6 @@
 /* Copyright (C) 1989, 2000 Aladdin Enterprises.  All rights reserved. */
 
-/*$Id: ansi2knr.c,v 1.14 2003/09/06 05:36:56 eggert Exp $*/
+/*$Id$*/
 /* Convert ANSI C function definitions to K&R ("traditional C") syntax */
 
 /*
